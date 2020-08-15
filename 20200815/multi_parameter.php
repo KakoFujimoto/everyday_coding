65c06bb3bc16b83c16663d21b00dc6c7f9089ada
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-08-15 14:36:30 +0900

    複数の値をフォームパラメータとして渡す場合

---
+++ 20200815/multi_parameter.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<!-- 7-6 -->
+
+<body>
+    <form action="select.php" method="POST">
+        <select name="color[]" id="">
+            <option value="red">RED</option>
+            <option value="blue">BLUE</option>
+            <option value="yellow">YELLOW</option>
+            <option value="green">GREEN</option>
+        </select>
+        <input type="submit" name="submit">
+    </form>
+</body>
+
+</html>
