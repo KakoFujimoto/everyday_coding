30c0570e1d698401b78297fee19423d3df2120e1
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-08-16 12:19:09 +0900

    サブクラスのコンストラクタ

---
+++ 20200816/constructor.php
@@ -0,0 +1,37 @@
+<?php
+//6-7
+
+class Super
+{
+    private $data;
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+}
+
+class Sub extends Super
+{
+    private $param;
+
+    public function getParam()
+    {
+        return $this->param;
+    }
+
+    public function __construct($data, $param)
+    {
+        $this->data = $data;
+        $this->param = $param;
+    }
+}
+
+$obj = new Sub('Red', 'Blue');
+print 'Data:' . $obj->getData() . '\n';
+print 'Param:' . $obj->getParam();
