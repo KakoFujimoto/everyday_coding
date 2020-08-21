60f7814c32f4738224edafb3975d57d60665d8d9
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-08-21 20:34:45 +0900

    login.phpエラー表示

--- sns/public_html/login.php
+++ sns/public_html/login.php
@@ -4,3 +4,22 @@ require_once(__DIR__ . '/../config/config.php');
 
 echo 'login screen';
 exit;
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <p><input type="text" name="email" placeholder="email"></p>
+        <p><input type="password" name="password" placeholder="password"></p>
+        <p><input type="submit" value="submit"></p>
+    </form>
+</body>
+
+</html>
