9090c7cbc2f9892b29dd0fa27ec2202f675c4944
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-08-23 18:20:20 +0900

    エラー解決

--- sns/lib/Controller/Signup.php
+++ sns/lib/Controller/Signup.php
@@ -35,10 +35,10 @@ class Signup extends \MyApp\Controller
     private function _validate()
     {
         if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-            throw new \Myapp\Exception\InvalidEmail();
+            throw new \MyApp\Exception\InvalidEmail();
         }
-        if (!preg_match('/\A[a-zA-Z0-9]+ \z/', $_POST['password'])) {
-            throw new \Myapp\Exception\InvalidPassword();
+        if (!preg_match('/\A[a-zA-Z0-9]+\z/', $_POST['password'])) {
+            throw new \MyApp\Exception\InvalidPassword();
         }
     }
 }
