fcd8ccf69adbd1296957f011966bdecb8f555daa
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-09-01 21:15:44 +0900

    IndexのViewを作成

--- sns/public_html/index.php
+++ sns/public_html/index.php
@@ -4,6 +4,27 @@ require_once(__DIR__ . '/../config/config.php');
 
 
 var_dump($_SESSION['me']);
-// $app = new MyApp\Controller\Index();
+?>
+<!DOCTYPE html>
+<html lang="ja">
 
-// $app->run();
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <form action="logout.php" method="post" id="logout">
+        xxxx@gmail.com <input type="submit" value="log out">
+        <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+    </form>
+    <h1>users</h1>
+    <ul>
+        <li>dummy</li>
+        <li>dummy</li>
+        <li>dummy</li>
+    </ul>
+</body>
+
+</html>
