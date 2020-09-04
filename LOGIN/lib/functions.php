8c8b1d4d859ec60fe1c7b997f466508228fb5bcc
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-09-04 22:04:09 +0900

    h関数検証コード

---
+++ LOGIN/lib/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function h($s)
+{
+    return htmlspecialchars($s, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+}
+
+
+echo h("");
+
+echo htmlspecialchars("'", ENT_QUOTES | ENT_HTML5, 'UTF-8');
+
+echo htmlspecialchars_decode('&#039;', ENT_QUOTES);
