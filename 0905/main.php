9dbfef3884b94fdba793e8199aaceb7ff1443247
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-09-05 12:01:20 +0900

    クラスの定義

--- 0905/main.php
+++ 0905/main.php
@@ -1,17 +1,29 @@
 <?php
 
-$posts = [];
-// $post[0] = 'hello';
-$posts[0] = ['text' => 'hello', 'likes' => 0];
-$posts[1] = ['text' => 'hello again', 'likes' => 0];
-// $post[1] = 'lemon';
-
-// print_r($posts);
-
-function show($hoge)
+class Post
 {
-    printf('%s (%d)' . PHP_EOL, $hoge['text'], $hoge['likes']);
+    public $text;
+    public $likes;
+
+    public function show()
+    {
+        printf('%s (%d)' . PHP_EOL, $this->text, $this->likes);
+    }
 }
 
-show($posts[0]);
-show($posts[1]);
+
+// $posts = [];
+// // $post[0] = 'hello';
+// $posts[0] = ['text' => 'hello', 'likes' => 0];
+// $posts[1] = ['text' => 'hello again', 'likes' => 0];
+// // $post[1] = 'lemon';
+
+// // print_r($posts);
+
+// function show($hoge)
+// {
+//     printf('%s (%d)' . PHP_EOL, $hoge['text'], $hoge['likes']);
+// }
+
+// show($posts[0]);
+// show($posts[1]);
