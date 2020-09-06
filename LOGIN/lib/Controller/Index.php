0d1d91ec85e1e4365c9f7e117ad27d39d752ae07
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-09-06 20:18:56 +0900

    IndexのController作成

---
+++ LOGIN/lib/Controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MyApp\Controller;
+
+class Index extends \MyApp\Controller
+{
+    public function run()
+    {
+        if (!$this->isLoggedIn()) {
+            //login
+        }
+    }
+}
