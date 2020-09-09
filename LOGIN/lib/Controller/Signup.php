997e0f33edb2dfb8077802e8429edf376b0317ce
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-09-09 13:52:48 +0900

    投稿時の処理の実装

--- LOGIN/lib/Controller/Signup.php
+++ LOGIN/lib/Controller/Signup.php
@@ -15,4 +15,28 @@ class Signup extends \MyApp\Controller
             $this->postProcess();
         }
     }
+
+    protected function postProcess()
+    {
+        // validate
+        try {
+            $this->_validate();
+        } catch (\MyApp\Exception\InvalidEmail $e) {
+        } catch (\MyApp\Exception\InvalidPassword $e) {
+        }
+        // create user
+
+        //redirect to
+
+    }
+
+    private function _validate()
+    {
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            throw new \MyApp\Exception\InvalidEmail();
+        }
+        if (!preg_match('/\A[a-zA-Z0-9]+\z/', $_POST['password'])) {
+            throw new \MyApp\Exception\InvalidPassword();
+        }
+    }
 }
