cf05488395c6cffe78229f52b1ef4e72f6182338
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-09-30 20:56:19 +0900

    htmlを作成

---
+++ quiz/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>Q.花沢類はどれ？</h1>
+    </div>
+    <ul>
+        <li class="answer">1</li>
+        <li class="answer">2</li>
+        <li class="answer">3</li>
+        <li class="answer">4</li>
+    </ul>
+    <div id="btn" class="disabled">次の問題へ</div>
+</body>
+
+</html>
