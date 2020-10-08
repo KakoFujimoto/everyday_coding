0ac12378ab181414f8c7fede8c734c3c858952de
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-10-08 19:28:06 +0900

    正答を選択できる状態にした

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -3,16 +3,26 @@ $(function () {
 
     $('.answer').on('click', function () {
         var $selected = $(this);
+        $selected.addClass('selected');
         var answer = $selected.text();
 
         $.post('/_answer.php', {
 
         }).done(function (res) {
+            $('.answer').each(function () {
+                if ($(this).text() === res.correct_answer) {
+                    $(this).addClass('correct');
+                } else {
+                    $(this).addClass('wrong');
+                }
+            });
             // alert(res.correct_answer);
             if (answer === res.correct_answer) {
                 // correct!
+                $selected.text(answer + ' ... CORRECT!');
             } else {
                 // wrong!
+                $selected.text(answer + ' ... WRONG!');
             }
         });
     });
